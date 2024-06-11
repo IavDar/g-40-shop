983e3e61bc40b837f1a40865ec73c1cc64bfe09c
IavDar <elcandess@gmail.com>
2024-06-11 20:44:30 +0200

    lesson 14

--- src/main/java/de/ait_tr/g_40_shop/service/ProductServiceImpl.java
+++ src/main/java/de/ait_tr/g_40_shop/service/ProductServiceImpl.java
@@ -5,6 +5,8 @@ import de.ait_tr.g_40_shop.domain.entity.Product;
 import de.ait_tr.g_40_shop.repository.ProductRepository;
 import de.ait_tr.g_40_shop.service.interfaces.ProductService;
 import de.ait_tr.g_40_shop.service.mapping.ProductMappingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    private Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
     private final ProductRepository repository;
     private final ProductMappingService mappingService;
 
@@ -38,11 +42,27 @@ public class ProductServiceImpl implements ProductService {
                 .toList();
     }
 
+//    демонстрация логирования на разные уровни:
+//    @Override
+//    public ProductDto getById(Long id) {
+//
+//        logger.info("Method getById called with parameter {}", id);
+//        logger.warn("Method getById called with parameter {}", id);
+//        logger.error("Method getById called with parameter {}", id);
+//
+//        Product product = repository.findById(id).orElse(null);
+//        if (product == null || !product.isActive()) {
+//            return null;
+//        }
+//        return mappingService.mapEntityToDto(product);
+//    }
+
     @Override
     public ProductDto getById(Long id) {
+
         Product product = repository.findById(id).orElse(null);
         if (product == null || !product.isActive()) {
-            return null;
+            throw new RuntimeException("Product not found");
         }
         return mappingService.mapEntityToDto(product);
     }
